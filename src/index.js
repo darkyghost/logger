ce24aedfe37c16994f9fd6c1118afe3414d1ac40
Rxflex <andreysuew@gmail.com>
2022-12-14 13:51:45 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import chalk from 'chalk';
+class logger {
+    warn(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.bgYellow('[WARN]')}: ${text}`);
+    }
+    log(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.bgMagenta('[LOG]')}: ${text}`);
+    }
+    sys(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.inverse('[SYSTEM]')}: ${text}`);
+    }
+    error(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.bgRed('[ERROR]')}: ${text}`);
+    }
+    start(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.bgMagenta(`[${pjson.name}]`)}: ${text}`);
+    }
+    request(text, pjson) {
+        let currentdate = new Date().toLocaleString('en-US', {timeZone: 'Europe/Kiev'}).replace(/T/, ' ').replace(/\..+/, '');
+        console.log(`${chalk.blue(currentdate)} | ${chalk.green(`[REQUEST]`)}: ${text}`);
+    }
+}
+
+
+export {logger};
